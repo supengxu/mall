d9b4246fde1d5bd852d7c785357d28afee9ebf66
supeng <xspdebug@gmail.com>
2019-07-18 16:43:57 +0800

    [feat](mapper、service): 用 mybatis gengerator生成的Pojo和mapper

--- src/main/java/com/xsp/mall/mapper/PermissionMapper.java
+++ src/main/java/com/xsp/mall/mapper/PermissionMapper.java
@@ -5,7 +5,7 @@ import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
-@Mapper
+
 public interface PermissionMapper {
     /**
      *  获取该用户的所有权限
---
+++ src/main/java/com/xsp/mall/mapper/PmsBrandMapper.java
@@ -0,0 +1,38 @@
+package com.xsp.mall.mapper;
+
+import com.xsp.mall.pojo.PmsBrand;
+import com.xsp.mall.pojo.PmsBrandExample;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+@Mapper
+public interface PmsBrandMapper {
+    long countByExample(PmsBrandExample example);
+
+    int deleteByExample(PmsBrandExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(PmsBrand record);
+
+    int insertSelective(PmsBrand record);
+
+    List<PmsBrand> selectByExampleWithBLOBs(PmsBrandExample example);
+
+    List<PmsBrand> selectByExample(PmsBrandExample example);
+
+    PmsBrand selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") PmsBrand record, @Param("example") PmsBrandExample example);
+
+    int updateByExampleWithBLOBs(@Param("record") PmsBrand record, @Param("example") PmsBrandExample example);
+
+    int updateByExample(@Param("record") PmsBrand record, @Param("example") PmsBrandExample example);
+
+    int updateByPrimaryKeySelective(PmsBrand record);
+
+    int updateByPrimaryKeyWithBLOBs(PmsBrand record);
+
+    int updateByPrimaryKey(PmsBrand record);
+}
